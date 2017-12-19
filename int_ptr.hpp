7089a0e85bb7c6349ea6da4ca63f0d6d9d55bdc9
Tristan Brindle <t.c.brindle@gmail.com>
2017-12-19 17:10:58 +0000

    Add solution

--- int_ptr.hpp
+++ int_ptr.hpp
@@ -7,13 +7,12 @@
 struct int_ptr {
     // Default constructor
     // Requirement: sets ptr to an appropriate default value
-    int_ptr()
-    {
-    }
+    int_ptr() = default;
 
     // Int constructor
     // Requirement: creates a new dynamically-allocated int with value i and assigns it to ptr
     explicit int_ptr(int i)
+        : ptr(new int{i})
     {
     }
 
@@ -21,18 +20,29 @@ struct int_ptr {
     // Requirement: deallocates ptr
     ~int_ptr()
     {
+        delete ptr;
     }
 
     // Copy constructor
     // Requirement: allocates a new dynamically-allocated int with the same value as other
     int_ptr(const int_ptr& other)
     {
+        if (other.ptr) {
+            ptr = new int{*other.ptr};
+        }
     }
 
     // Copy assignment operator
     // Requirement: sets the value of our int to the same as the value of other
     int_ptr& operator=(const int_ptr& other)
     {
+        if (this != &other) {
+            delete ptr;
+            ptr = nullptr;
+            if (other.ptr) {
+                ptr = new int{*other.ptr};
+            }
+        }
         return *this;
     }
 
@@ -41,26 +51,28 @@ struct int_ptr {
     // Requirement: returns true iff ptr is allocated
     explicit operator bool() const
     {
-        return false;
+        return ptr != nullptr;
     }
 
     // Dereference operator
     // Requirement: returns a reference to our contained object
     // Note: the return value of the prototype is not correct!
-    int operator*() const
+    int& operator*() const
     {
-        return 0;
+        return *ptr;
     }
 
     // Requirement: returns the contained pointer
     int* get() const
     {
-        return nullptr;
+        return ptr;
     }
 
     // Requirement: replaces the contained pointer with other_ptr
     void reset(int* other_ptr = nullptr)
     {
+        delete ptr;
+        ptr = other_ptr;
     }
 
 
